26f5e73e088536b7c9e3a59160083ddb5990948a
Kayden Zhou <kaydenzhoU@gmail.com>
2025-08-20 09:54:32 -0700

    organized cells into a grid like formation
    this will be what users draw on

--- script.js
+++ script.js
@@ -2,9 +2,20 @@ let gridWidth = 16
 
 const container = document.querySelector("#container")
 
-for (let i = 0; i < (gridWidth ** 2); i++) {
-    const gridCell = document.createElement("div")
-    gridCell.classList.add("gridCell")
+function generateGrid(gridWidth) {
+    for (let i = 0; i < (gridWidth ** 2); i++) {
+        const gridCell = document.createElement("div")
+        gridCell.classList.add("gridCell")
 
-    container.appendChild(gridCell)
+        let cellWidth = 960 / gridWidth
+
+        gridCell.style.maxHeight = cellWidth + 'px'
+        gridCell.style.maxWidth = cellWidth + 'px'
+        gridCell.style.minHeight = cellWidth + 'px'
+        gridCell.style.minWidth = cellWidth + 'px'
+
+        container.appendChild(gridCell)
+    }
 }
+
+generateGrid(gridWidth)
