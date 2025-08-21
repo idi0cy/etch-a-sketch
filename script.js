7551432dd9db1ea1f762c2a88f5ce74be5b748f7
Kayden Zhou <kaydenzhoU@gmail.com>
2025-08-21 09:32:40 -0700

    buttons for changing grid added
    users can now customize the etch a sketch

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-let gridWidth = 16
+let gridWidth = 64
 
 const container = document.querySelector("#container")
 function generateGrid(gridWidth) {
     for (let i = 0; i < (gridWidth ** 2); i++) {
+
         const gridCell = document.createElement("div")
         gridCell.classList.add("gridCell")
 
@@ -14,14 +15,39 @@ function generateGrid(gridWidth) {
         gridCell.style.minWidth = cellWidth + 'px'
 
         gridCell.addEventListener('mouseover', function(){
-            gridCell.classList.toggle('coloured')
+            gridCell.classList.add('coloured')
         })
 
         container.appendChild(gridCell)
     }
 }
 
+const btn = document.querySelector('button')
+const restartBtn = document.querySelector('.refresh')
+
+btn.addEventListener('click', function changeGrid(){
+    gridWidth = parseInt(prompt("Please enter new grid size (positive integer below 100)."))
+
+    if (gridWidth > 0 && gridWidth < 101 && Number.isInteger(gridWidth)){
+        alert('Grid width was set to ' + gridWidth)
+    } else {
+        alert('invalid grid width. Please choose a positive integer below 100.')
+        gridWidth = 16
+    }
+
+    while (container.firstChild){
+        container.removeChild(container.firstChild)
+    }
+
+    generateGrid(gridWidth)
+})
+
+restartBtn.addEventListener('click', function refreshGrid(){
+    while (container.firstChild){
+        container.removeChild(container.firstChild)
+    }
+
+    generateGrid(gridWidth)
+})
+
 generateGrid(gridWidth)
-
-
-
